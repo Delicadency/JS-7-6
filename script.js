5907b77a16b0e833d46790b5355db5cc648cfa19
Agata Kamińska <164206709+Delicadency@users.noreply.github.com>
2024-05-24 19:55:25 +0000

    added fn selectItem, not finished

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 /* Stwórz funkcję selectItem tworzącą element <select> (użyj document.createElement), o identyfikatorze “level”, który będzie posiadał trzy elementy <option> z atrybutami value z cyframi od 1 do 3 i dodaj go do elementu <body>. Funkcja powinna także zwracać element <select>. Dodaj w środku tej funkcji obsługę zdarzenia change elementu <select>, która po zajściu zdarzenia wywoła funkcję window.alert z wybraną opcją z elementu <select>. a
  */
+const selectItem = () => {
+  const select = document.createElement("select");
+  select.id = "level";
+  select.innerHTML = `
+  <option value = "1">Poziom 1 </option>
+  <option value = "2">Poziom 2</option>
+  <option value = "3">Poziom 3</option>`;
+  document.body.appendChild(select);
+  return select;
+};
